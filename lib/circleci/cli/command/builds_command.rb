d3dfeb2be78b26d3c54aefed1d2af0707014de53
YKliv <75178780+YKliv@users.noreply.github.com>
2021-06-18 12:03:24 +0000

    [EngCom] Public Pull Requests - 2.3-develop

--- lib/circleci/cli/command/builds_command.rb
+++ lib/circleci/cli/command/builds_command.rb
@@ -3,3 +3,24 @@
 
 module CircleCI
   module CLI
+    module Command
+      class BuildsCommand < BaseCommand
+        class << self
+          def run(options)
+            setup_token
+            project_name = project_name(options)
+            username, reponame = project_name.split('/')
+            branch = branch_name(options)
+            builds = if branch
+                       Response::Build.branch(username, reponame, branch)
+                     else
+                       Response::Build.all(username, reponame)
+                     end
+
+            say Printer::BuildPrinter.new(builds, project_name, pretty: options.pretty).to_s
+          end
+        end
+      end
+    end
+  end
+end
