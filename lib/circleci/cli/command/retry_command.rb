e0e726cb86e0a10eaab5ae7f36f1961ca638965d
YKliv <75178780+YKliv@users.noreply.github.com>
2021-07-07 15:37:13 +0000

    misc update

--- lib/circleci/cli/command/retry_command.rb
+++ lib/circleci/cli/command/retry_command.rb
@@ -12,3 +12,29 @@ module CircleCI
             number = build_number_for(username, reponame, options)
 
             build = Response::Build.retry(username, reponame, number)
+
+            if build&.username
+              say "build #{username}/#{reponame} #{build.build_number} is triggered"
+            else
+              say "failed to trigger #{username}/#{reponame} #{number}"
+            end
+          end
+
+          private
+
+          def build_number_for(username, reponame, options)
+            if options.last
+              get_last_build_number(username, reponame)
+            else
+              build_number(options)
+            end
+          end
+
+          def get_last_build_number(username, reponame)
+            Response::Build.failed(username, reponame).map(&:build_number).max
+          end
+        end
+      end
+    end
+  end
+end
