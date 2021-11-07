8d1cf3d510d28e739e3d67747f1872e8d2669ece
YKliv <75178780+YKliv@users.noreply.github.com>
2021-11-07 20:32:51 +0000

    add dist to gitignore

--- lib/circleci/cli/command/watch_command.rb
+++ lib/circleci/cli/command/watch_command.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 require 'circleci/cli/command/watch_command/build_repository'
-require 'circleci/cli/command/watch_com
+require 'circleci/cli/command/watch_command/build_watcher'
+
+module CircleCI
+  module CLI
+    modu
