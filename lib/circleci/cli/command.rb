9f080abee152ff022126d48079fdaccb6accfcbe
YKliv <75178780+YKliv@users.noreply.github.com>
2021-12-20 08:35:01 +0000

    fix build on linux

--- lib/circleci/cli/command.rb
+++ lib/circleci/cli/command.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 require 'circleci/cli/command/base_command'
-require 'circleci/cli/command/pro
+require 'circleci/cli/command/projects_command'
+require 'circleci/cli/comma
