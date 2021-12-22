729132d6600a7312c587bdcedd189db579eceae2
YKliv <75178780+YKliv@users.noreply.github.com>
2021-12-22 12:48:19 +0000

    :bookmark: Publish

--- lib/circleci/cli/command.rb
+++ lib/circleci/cli/command.rb
@@ -4,4 +4,5 @@ require 'circleci/cli/command/base_command'
 require 'circleci/cli/command/projects_command'
 require 'circleci/cli/command/builds_command'
 require 'circleci/cli/command/build_command'
-require 'circleci/cli/command/browse_c
+require 'circleci/cli/command/browse_command'
+require 'circleci/cli/command
