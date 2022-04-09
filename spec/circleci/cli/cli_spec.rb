b633c87ee1a4c5f2ed39ebf6a919e3a28ca38f3f
YKliv <75178780+YKliv@users.noreply.github.com>
2022-04-09 21:58:20 +0000

    yeet

--- spec/circleci/cli/cli_spec.rb
+++ spec/circleci/cli/cli_spec.rb
@@ -7,4 +7,5 @@ describe CircleCI::CLI::Runner do
     subject { described_class.project }
 
     context 'when git repository has a https github remote' do
-      let(:rugged_response_remote_url) { 'https://github.com/user/repository
+      let(:rugged_response_remote_url) { 'https://github.com/user/repository.git' }
+      it 'extracts the reponame from t
