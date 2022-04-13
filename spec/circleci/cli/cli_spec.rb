04b9a6aae235216f0890c7afb80ff1ff70eb9607
YKliv <75178780+YKliv@users.noreply.github.com>
2022-04-13 10:17:38 +0000

    new report

--- spec/circleci/cli/cli_spec.rb
+++ spec/circleci/cli/cli_spec.rb
@@ -16,4 +16,6 @@ describe CircleCI::CLI::Runner do
     context 'when git repository has a github remote' do
       let(:rugged_response_remote_url) { 'git@github.com:user/repository.git' }
       it 'extracts the reponame from the origin url' do
-        expect(s
+        expect(subject).to eq('user/repository')
+      end
+   
