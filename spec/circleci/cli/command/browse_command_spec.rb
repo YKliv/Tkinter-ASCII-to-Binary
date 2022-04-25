fe6423d0d5ec373ced56bf02cdaba4583c669eec
YKliv <75178780+YKliv@users.noreply.github.com>
2022-04-25 08:21:25 +0000

    Update namespaces

--- spec/circleci/cli/command/browse_command_spec.rb
+++ spec/circleci/cli/command/browse_command_spec.rb
@@ -3,4 +3,6 @@
 require 'spec_helper'
 
 describe CircleCI::CLI::Command::BrowseCommand, type: :command do
-  shared_examples_for 'a command open
+  shared_examples_for 'a command opens browser' do
+    it 'should open browser' do
+      a
