acebb0d4b5e45c1744a31c1537566be96f15f198
YKliv <75178780+YKliv@users.noreply.github.com>
2022-04-26 14:24:41 +0000

    Updated service API models for release.

--- spec/circleci/cli/command/browse_command_spec.rb
+++ spec/circleci/cli/command/browse_command_spec.rb
@@ -6,4 +6,5 @@ describe CircleCI::CLI::Command::BrowseCommand, type: :command do
   shared_examples_for 'a command opens browser' do
     it 'should open browser' do
       allow(CircleCI::CLI::Command::BrowseCommand).to receive(:say)
-      expect(Launchy).to receive(:open)
+      expect(Launchy).to receive(:open).with(expected_url)
+      CircleCI::CLI::Command:
