d9eba4700ce64f520ae6f476afb82f89b0c89b96
YKliv <75178780+YKliv@users.noreply.github.com>
2022-05-06 08:49:07 +0000

    fixed everything

--- spec/circleci/cli/command/build_command_spec.rb
+++ spec/circleci/cli/command/build_command_spec.rb
@@ -3,4 +3,7 @@
 require 'spec_helper'
 
 describe CircleCI::CLI::Command::BuildCommand, type: :command do
-  shared_examples_for 'a command show build informati
+  shared_examples_for 'a command show build information' do
+    let(:expected_output) do
+      <<~EXPECTED
+  
