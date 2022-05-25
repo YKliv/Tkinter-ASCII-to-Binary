e0ca56246aca7f221055f0ca162a7ade7d970ce2
YKliv <75178780+YKliv@users.noreply.github.com>
2022-05-25 09:44:39 +0000

    Ajout des fichiers de langue française admin.

--- spec/circleci/cli/command/builds_command_spec.rb
+++ spec/circleci/cli/command/builds_command_spec.rb
@@ -3,4 +3,6 @@
 require 'spec_helper'
 
 describe CircleCI::CLI::Command::BuildsCommand, type: :command do
-  shared_examples_for 'a command show builds info
+  shared_examples_for 'a command show builds information' do
+    let(:expected_output) do
+      <<~EXP
