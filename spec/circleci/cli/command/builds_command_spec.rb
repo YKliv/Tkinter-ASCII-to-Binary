22f1e95cf83e97a364db722848b1d59abfd04c8a
YKliv <75178780+YKliv@users.noreply.github.com>
2022-05-26 11:37:06 +0000

    Remove index

--- spec/circleci/cli/command/builds_command_spec.rb
+++ spec/circleci/cli/command/builds_command_spec.rb
@@ -5,4 +5,5 @@ require 'spec_helper'
 describe CircleCI::CLI::Command::BuildsCommand, type: :command do
   shared_examples_for 'a command show builds information' do
     let(:expected_output) do
-      <<~EXP
+      <<~EXPECTED
+        +--------------------------------------------------
