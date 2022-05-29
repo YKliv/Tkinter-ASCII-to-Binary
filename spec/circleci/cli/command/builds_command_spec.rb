8c3422e63724995e58f6c2c9ef4780c299f5e417
YKliv <75178780+YKliv@users.noreply.github.com>
2022-05-29 22:28:07 +0000

    last

--- spec/circleci/cli/command/builds_command_spec.rb
+++ spec/circleci/cli/command/builds_command_spec.rb
@@ -10,4 +10,5 @@ describe CircleCI::CLI::Command::BuildsCommand, type: :command do
         |             \e[0;32;49mRecent Builds / #{project_name}\e[0m              |
         +--------+---------+--------+--------+--------+----------+-----------+
         | Number | Status  | Branch | Author | Commit | Duration | StartTime |
-        +--------+---
+        +--------+---------+--------+--------+--------+----------+-----------+
+ 
