f82d0ba2ecef1e131e853e68be229789c1dacd58
YKliv <75178780+YKliv@users.noreply.github.com>
2022-06-23 21:57:49 +0000

    Bump version to 0.9.7

--- spec/circleci/cli/command/retry_command_spec.rb
+++ spec/circleci/cli/command/retry_command_spec.rb
@@ -4,4 +4,5 @@ require 'spec_helper'
 
 describe CircleCI::CLI::Command::RetryCommand, type: :command do
   shared_examples_for 'a command retries build' do
-    it 'should retry 
+    it 'should retry build' do
+      allow(CircleCI::CLI::Command::RetryCom
