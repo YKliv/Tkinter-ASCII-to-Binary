144e45184b77ef1f5b02c6f5eb13c126787641f7
YKliv <75178780+YKliv@users.noreply.github.com>
2022-06-24 14:27:38 +0000

    Ignore .idea

--- spec/circleci/cli/command/retry_command_spec.rb
+++ spec/circleci/cli/command/retry_command_spec.rb
@@ -6,4 +6,4 @@ describe CircleCI::CLI::Command::RetryCommand, type: :command do
   shared_examples_for 'a command retries build' do
     it 'should retry build' do
       allow(CircleCI::CLI::Command::RetryCommand).to receive(:say) { nil }
-      expect(CircleCI::CLI::
+      expect(CircleCI::CLI::Command::RetryCommand).to receive(:say).with(exp
