79485f163828abb39d8351d5fb5993b72e005a4d
YKliv <75178780+YKliv@users.noreply.github.com>
2022-06-24 20:39:15 +0000

    intellidemand: Add boost pulse capability

--- spec/circleci/cli/command/retry_command_spec.rb
+++ spec/circleci/cli/command/retry_command_spec.rb
@@ -6,4 +6,5 @@ describe CircleCI::CLI::Command::RetryCommand, type: :command do
   shared_examples_for 'a command retries build' do
     it 'should retry build' do
       allow(CircleCI::CLI::Command::RetryCommand).to receive(:say) { nil }
-      expect(CircleCI::CLI::Command::RetryCommand).to receive(:say).with(exp
+      expect(CircleCI::CLI::Command::RetryCommand).to receive(:say).with(expected_output.strip)
+      CircleCI::CLI::Command::Ret
