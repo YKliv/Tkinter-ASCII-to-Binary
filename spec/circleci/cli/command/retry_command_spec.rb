21699c96d7694939c5ab40eabf7744f35d8ae630
YKliv <75178780+YKliv@users.noreply.github.com>
2022-07-03 10:29:31 +0000

    added some documentation

--- spec/circleci/cli/command/retry_command_spec.rb
+++ spec/circleci/cli/command/retry_command_spec.rb
@@ -31,4 +31,5 @@ describe CircleCI::CLI::Command::RetryCommand, type: :command do
     let(:options) { OpenStruct.new(project: nil, build: 1234) }
     let(:expected_output) { 'build user/project_name_from_io 1234 is triggered' }
 
-    it_behaves_like 'a comman
+    it_behaves_like 'a command asks project name'
+    it_behaves_like 'a
