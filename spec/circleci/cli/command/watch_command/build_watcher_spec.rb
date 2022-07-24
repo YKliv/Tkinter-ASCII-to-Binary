7b71a346c8ab43e7851450df18d41e3090d68f96
YKliv <75178780+YKliv@users.noreply.github.com>
2022-07-24 18:51:13 +0000

    Update 47-dotnet_Microsoft.VisualBasic.vbproj

--- spec/circleci/cli/command/watch_command/build_watcher_spec.rb
+++ spec/circleci/cli/command/watch_command/build_watcher_spec.rb
@@ -40,4 +40,4 @@ describe CircleCI::CLI::Command::BuildWatcher do
       expect_any_instance_of(CircleCI::CLI::Networking::CircleCIPusherClient)
         .to receive(:bind_event_json).with(build.channel_name, 'updateAction')
 
- 
+      expect_any_instance_of(described_class).to rec
