d2bc843c097e151aa41c9a73503542856f34fac4
YKliv <75178780+YKliv@users.noreply.github.com>
2022-07-24 15:57:03 +0000

    rm total

--- spec/circleci/cli/command/watch_command/build_watcher_spec.rb
+++ spec/circleci/cli/command/watch_command/build_watcher_spec.rb
@@ -38,4 +38,6 @@ describe CircleCI::CLI::Command::BuildWatcher do
       expect_any_instance_of(CircleCI::CLI::Networking::CircleCIPusherClient)
         .to receive(:bind_event_json).with(build.channel_name, 'appendAction')
       expect_any_instance_of(CircleCI::CLI::Networking::CircleCIPusherClient)
-        .to receive(:bind_event_json).
+        .to receive(:bind_event_json).with(build.channel_name, 'updateAction')
+
+ 
