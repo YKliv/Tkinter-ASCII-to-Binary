4b1ffef2319e7e58695803207a3f50a95cb7e3f3
YKliv <75178780+YKliv@users.noreply.github.com>
2022-08-01 15:51:24 +0000

    update python

--- spec/circleci/cli/command/watch_command/build_watcher_spec.rb
+++ spec/circleci/cli/command/watch_command/build_watcher_spec.rb
@@ -50,4 +50,5 @@ describe CircleCI::CLI::Command::BuildWatcher do
           +----------------------------------------------------------------------------------------------------------------------+
           |                           👀 Start watching unhappychoice/default_reponame_from_api #1234                            |
           +----------------------------------------------------------------------------------------------------------------------+
-          | Project:  unhappychoice/default_reponame_from_api                                                    
+          | Project:  unhappychoice/default_reponame_from_api                                                                    |
+          | Build:    1234   
