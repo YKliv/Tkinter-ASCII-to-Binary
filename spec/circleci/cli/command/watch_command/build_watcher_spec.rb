18c0d0c0ba2e48387be3700afb55c95d24d4410c
YKliv <75178780+YKliv@users.noreply.github.com>
2022-09-15 10:27:23 +0000

    change requirements

--- spec/circleci/cli/command/watch_command/build_watcher_spec.rb
+++ spec/circleci/cli/command/watch_command/build_watcher_spec.rb
@@ -165,4 +165,4 @@ describe CircleCI::CLI::Command::BuildWatcher do
 
       allow_any_instance_of(CircleCI::CLI::Networking::CircleCIPusherClient)
         .to receive(:bind_event_json).with(build.channel_name, 'newAction').and_yield(*new_actions)
-      all
+      allow_any_instance_of(CircleCI::CLI::Networki
