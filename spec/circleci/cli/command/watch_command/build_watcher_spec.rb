e02c23863e2ab4daf21f5791038900ef3ac76a8e
YKliv <75178780+YKliv@users.noreply.github.com>
2022-09-16 19:50:25 +0000

    regenerate html

--- spec/circleci/cli/command/watch_command/build_watcher_spec.rb
+++ spec/circleci/cli/command/watch_command/build_watcher_spec.rb
@@ -167,4 +167,5 @@ describe CircleCI::CLI::Command::BuildWatcher do
         .to receive(:bind_event_json).with(build.channel_name, 'newAction').and_yield(*new_actions)
       allow_any_instance_of(CircleCI::CLI::Networking::CircleCIPusherClient)
         .to receive(:bind_event_json).with(build.channel_name, 'appendAction').and_yield(*append_actions)
-      allow_any_instance_o
+      allow_any_instance_of(CircleCI::CLI::Networking::CircleCIPusherClient)
+        
