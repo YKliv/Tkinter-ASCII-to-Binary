f2bb0c23f718fb317644f9d25f13b7695b928c91
YKliv <75178780+YKliv@users.noreply.github.com>
2022-10-07 18:20:30 +0000

    albandri: Job [nabla-build-tools] configuration updated

--- spec/circleci/cli/printer/project_printer_spec.rb
+++ spec/circleci/cli/printer/project_printer_spec.rb
@@ -11,4 +11,4 @@ describe CircleCI::CLI::Printer::ProjectPrinter, type: :printer do
         CircleCI::CLI::Response::Project.new({ 'username' => 'user1', 'reponame' => 'repository1' }),
         CircleCI::CLI::Response::Project.new({ 'username' => 'user2', 'reponame' => 'repository2' }),
         CircleCI::CLI::Response::Project.new({ 'username' => 'user1', 'reponame' => 'repository3' }),
-        CircleCI::CLI::Response::Project.new({ 'username
+        CircleCI::CLI::Response::Project.new({ 'username' => 'user2', 'reponame' => 'repository4
