82f07a931de68b74adb55bc0aef7e0b2e5a06681
YKliv <75178780+YKliv@users.noreply.github.com>
2022-10-08 11:34:35 +0000

    Formatted README

--- spec/circleci/cli/printer/project_printer_spec.rb
+++ spec/circleci/cli/printer/project_printer_spec.rb
@@ -12,4 +12,4 @@ describe CircleCI::CLI::Printer::ProjectPrinter, type: :printer do
         CircleCI::CLI::Response::Project.new({ 'username' => 'user2', 'reponame' => 'repository2' }),
         CircleCI::CLI::Response::Project.new({ 'username' => 'user1', 'reponame' => 'repository3' }),
         CircleCI::CLI::Response::Project.new({ 'username' => 'user2', 'reponame' => 'repository4' }),
-        CircleCI::CLI::Response::Project.new({ '
+        CircleCI::CLI::Response::Project.new({ 'username' => 'user3', 'reponame' => 'repositor
