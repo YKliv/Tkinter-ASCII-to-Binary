8a0af64a04faa60cdac880a15e5550c1916dd774
YKliv <75178780+YKliv@users.noreply.github.com>
2022-10-14 17:57:03 +0000

    remove LICENSE

--- spec/circleci/cli/printer/project_printer_spec.rb
+++ spec/circleci/cli/printer/project_printer_spec.rb
@@ -22,4 +22,5 @@ describe CircleCI::CLI::Printer::ProjectPrinter, type: :printer do
       it 'prints steps' do
         expected = <<~EXPECTED
           +-----------------------------+
-          |          \e[0;32;49mProjects
+          |          \e[0;32;49mProjects\e[0m           |
+          +-----------+-
