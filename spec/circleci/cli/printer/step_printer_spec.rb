0779fd0d6f1c77bc4c85a8f0dbb232719c8ae1e8
YKliv <75178780+YKliv@users.noreply.github.com>
2022-11-04 18:50:03 +0000

    contacto

--- spec/circleci/cli/printer/step_printer_spec.rb
+++ spec/circleci/cli/printer/step_printer_spec.rb
@@ -4,4 +4,7 @@ require 'spec_helper'
 
 describe CircleCI::CLI::Printer::StepPrinter, type: :printer do
   describe '#to_s' do
-    subject { described_class.new(
+    subject { described_class.new(steps, pretty: pretty).to_s }
+
+    before do
+ 
