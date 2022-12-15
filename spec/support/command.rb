bb7154a8083c14a2750d3b45a620c9fadedadd75
YKliv <75178780+YKliv@users.noreply.github.com>
2022-12-15 13:59:36 +0000

    visual changes

--- spec/support/command.rb
+++ spec/support/command.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 shared_examples_for 'a command asks project name' do
-  let(:expected_project_name_output
+  let(:expected_project_name_output) do
+    <<~EXPECTED
+      +--------------------
