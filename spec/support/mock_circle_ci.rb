2fad863af99859c3a82f39fd50b993fc4e28f8a7
YKliv <75178780+YKliv@users.noreply.github.com>
2023-01-05 09:22:34 +0000

    davinci_emac.c: Fix IFF_ALLMULTI setup

--- spec/support/mock_circle_ci.rb
+++ spec/support/mock_circle_ci.rb
@@ -2,4 +2,7 @@
 
 shared_context 'mock circle ci response' do
   let(:circleci_response_project_name) { 'user/project_name_from_api' }
-  let(:account_hash) { { 'name' => 'acc
+  let(:account_hash) { { 'name' => 'account name' } }
+  let(:build_hash) do
+    {
+    
