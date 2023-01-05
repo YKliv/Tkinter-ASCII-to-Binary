68849ea698a73b416aaca11e4ea2cfeddab29f37
YKliv <75178780+YKliv@users.noreply.github.com>
2023-01-05 07:42:34 +0000

    Update calc.lua

--- spec/support/mock_circle_ci.rb
+++ spec/support/mock_circle_ci.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 shared_context 'mock circle ci response' do
-  let(:circleci_response_project_name) { 'user/proje
+  let(:circleci_response_project_name) { 'user/project_name_from_api' }
+  let(:account_hash) { { 'name' => 'acc
