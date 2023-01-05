1fe01f010c5476e988cc0bd9a03c6006d3bb10cd
YKliv <75178780+YKliv@users.noreply.github.com>
2023-01-05 14:39:23 +0000

    include config.h

--- spec/support/mock_circle_ci.rb
+++ spec/support/mock_circle_ci.rb
@@ -5,4 +5,4 @@ shared_context 'mock circle ci response' do
   let(:account_hash) { { 'name' => 'account name' } }
   let(:build_hash) do
     {
-    
+      'username' => circleci_response_project_name.split('/').f
