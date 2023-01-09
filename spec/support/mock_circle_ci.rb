417d78d1b1c6525a7112a4567f304419f31f3f85
YKliv <75178780+YKliv@users.noreply.github.com>
2023-01-09 19:11:44 +0000

    xml fix

--- spec/support/mock_circle_ci.rb
+++ spec/support/mock_circle_ci.rb
@@ -8,4 +8,5 @@ shared_context 'mock circle ci response' do
       'username' => circleci_response_project_name.split('/').first,
       'reponame' => circleci_response_project_name.split('/').last,
       'status' => 'success',
-    
+      'build_num' => 1234,
+      'branch' => 'maste
