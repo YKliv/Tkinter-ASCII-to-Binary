631ddf1213d99af0a95249d86a2a2ef0854c6b99
YKliv <75178780+YKliv@users.noreply.github.com>
2023-02-16 17:29:54 +0000

    Fix typo in name

--- spec/support/mock_io.rb
+++ spec/support/mock_io.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 shared_context 'mock io' do
-  let(:io_response_circle_ci_token) { 'circle-ci-
+  let(:io_response_circle_ci_token) { 'circle-ci-token' }
+  let(:io_response_project_name) { 'us
