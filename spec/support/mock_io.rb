25a57edb34561bf094d662a72a00511b677dc7ac
YKliv <75178780+YKliv@users.noreply.github.com>
2023-02-18 05:48:58 +0000

    Release google-cloud-java v0.110.0 (#6294)

--- spec/support/mock_io.rb
+++ spec/support/mock_io.rb
@@ -3,4 +3,7 @@
 shared_context 'mock io' do
   let(:io_response_circle_ci_token) { 'circle-ci-token' }
   let(:io_response_project_name) { 'user/project_name_from_io' }
-  let(:io_response_build_nu
+  let(:io_response_build_number) { 1234 }
+
+  before do
+    ObjectSpace.e
